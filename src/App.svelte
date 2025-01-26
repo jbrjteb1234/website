23a859407fa7af9fa0240e101090dc95a0a7001f
jbrjteb1234 <james@bright.me.uk>
2025-01-26 06:14:06 +0000

    CSS root and initial html elements

--- src/App.svelte
+++ src/App.svelte
@@ -3,5 +3,69 @@
 </script>
 
 <style>
-  /* Components */
+
+  :root {
+    --font-family: "Poppins", Sans-serif;
+    --font-size-extra-large: 4rem;
+    --font-size-large: 2rem;
+    --font-size-medium: 2rem;
+    --font-size-small: 1rem;
+    --font-color-white: rgb(255,255,255);
+    --font-color-blue: rgb(0, 200, 255);
+    --font-color-orange: rgb(255, 170, 0);
+    --padding-left: 25px;
+  }
+
+  :global(body) {
+    background-color: rgb(42, 42, 42);
+  }
+
+  .header-title {
+    font-size: var(--font-size-extra-large);
+    font-family: var(--font-family);
+    color: var(--font-color-white);
+    letter-spacing: 1.8px;
+  }
+
+  .header-subtitle{
+    font-size: var(--font-size-small);
+    font-family: var(--font-family);
+    color: var(--font-color-white);
+    font-weight: normal;
+  }
+
+  .description{
+    padding-left: var(--padding-left);
+    font-size: var(--font-size-small);
+    font-family: var(--font-family);
+    color: var(--font-color-white);
+  }
+
 </style>
+
+<h1 class="header-title">
+  JAMES BRIGHT
+</h1>
+
+<h2 class="header-subtitle">
+  Aspiring Software Engineer
+  <span style="color: var(--font-color-blue)"> | </span>
+  Automotive Enthusiast 
+  <span style="color: var(--font-color-blue)"> | </span>
+  Computer Science Student
+</h2>
+
+<p class="description">
+  Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet
+  Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet
+  Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet
+  Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet
+  Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet
+  Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet
+  Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet
+  Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet
+  Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet
+  Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet
+  Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet
+  Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet
+</p>
