5cbb73f4c486597a4abb580dd34694a78362d89c
jbrjteb1234 <james@bright.me.uk>
2025-01-26 17:56:58 +0000

    Created box style for headers

--- src/App.svelte
+++ src/App.svelte
@@ -6,36 +6,57 @@
 
   :root {
     --font-family: "Poppins", Sans-serif;
-    --font-size-extra-large: 4rem;
-    --font-size-large: 2rem;
-    --font-size-medium: 2rem;
-    --font-size-small: 1rem;
+    --font-family2: 'Fira Code', monospace;
+    --font-size-large: 5rem;
+    --font-size-medium: 3rem;
+    --font-size-small: 2rem;
+    --font-size-text: 1.5rem;
+    --font-size-sub-text: 1vw;
     --font-color-white: rgb(255,255,255);
     --font-color-blue: rgb(0, 200, 255);
     --font-color-orange: rgb(255, 170, 0);
-    --padding-left: 25px;
   }
 
   :global(body) {
-    background-color: rgb(42, 42, 42);
+    background-color: rgb(60, 60, 60);
+  }
+
+  .header-container{
+    display: flex;
+    width: 420px;
+    margin-left: 10%;
+    margin-top: 5%;
+    flex-direction: column;
+    align-items: center;
+    background-color: rgb(0,0,0);
+    box-shadow: 25px 25px 12px 0px rgba(0,0,0,0.75);
   }
 
   .header-title {
-    font-size: var(--font-size-extra-large);
+    font-size: var(--font-size-large);
     font-family: var(--font-family);
     color: var(--font-color-white);
     letter-spacing: 1.8px;
+    text-align: left;
+    margin-top: 5%;
+    padding-left: 5%;
+    width: 100%;
+    flex: 1;
   }
 
   .header-subtitle{
-    font-size: var(--font-size-small);
-    font-family: var(--font-family);
+    font-size: var(--font-size-text);
+    font-family: var(--font-family2);
     color: var(--font-color-white);
     font-weight: normal;
+    text-align: left;
+    margin-top: 5%;
+    padding-left: 5%;
+    width: 100%;
+    flex: 1;
   }
 
   .description{
-    padding-left: var(--padding-left);
     font-size: var(--font-size-small);
     font-family: var(--font-family);
     color: var(--font-color-white);
@@ -43,29 +64,17 @@
 
 </style>
 
-<h1 class="header-title">
-  JAMES BRIGHT
-</h1>
+<div class="header-container">
+  <h1 class="header-title">
+    JAMES<br>BRIGHT
+  </h1>
 
-<h2 class="header-subtitle">
-  Aspiring Software Engineer
-  <span style="color: var(--font-color-blue)"> | </span>
-  Automotive Enthusiast 
-  <span style="color: var(--font-color-blue)"> | </span>
-  Computer Science Student
-</h2>
-
-<p class="description">
-  Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet
-  Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet
-  Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet
-  Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet
-  Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet
-  Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet
-  Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet
-  Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet
-  Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet
-  Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet
-  Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet
-  Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet
-</p>
+  <h2 class="header-subtitle">
+    <span style="color: var(--font-color-blue)"> &gt </span>
+    Aspiring Software Engineer<br>
+    <span style="color: var(--font-color-blue)"> &gt </span>
+    Automotive Enthusiast<br>
+    <span style="color: var(--font-color-blue)"> &gt </span> 
+    Computer Science Student
+  </h2>
+</div>
