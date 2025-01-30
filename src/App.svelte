439b681b331f0228b06c13f8d3624e927e839fcd
jbrjteb1234 <james@bright.me.uk>
2025-01-30 22:55:48 +0000

    Added slight blue haze which follows mouse

--- src/App.svelte
+++ src/App.svelte
@@ -4,31 +4,56 @@
 
   onMount(() => {
 
-      function handleMouseMove(event){
-        const mouseX = event.clientX;
-        const mouseY = event.clientY;
+    function handleMouseMove(event){
+      const mouseX = event.clientX;
+      const mouseY = event.clientY;
 
-        const position = headerContainer.getBoundingClientRect();
-        const containerX = position.left + position.width/2;
+      const position = headerContainer.getBoundingClientRect();
+      const containerX = position.left + position.width/2;
 
-        if(headerContainer){
-          const differenceX = (mouseX - containerX)/25;
+      if(headerContainer){
+        const differenceX = (mouseX - containerX)/25;
 
-          const newX = Math.max(-50, Math.min(50, differenceX)); 
-          
-          headerContainer.style.transform = `translate(${newX}px, ${0}px)`;
-        }
-
+        const newX = Math.max(-50, Math.min(50, differenceX)); 
+        
+        headerContainer.style.transform = `translate(${newX}px, ${0}px)`;
       }
+    }
 
-      window.addEventListener('mousemove', handleMouseMove);
+  window.addEventListener('mousemove', handleMouseMove);
 
+  });
+
+  let haze;
+
+  onMount(() => {
+    function handleMouseMove(event){
+        
+      const mouseX = event.clientX;
+      const mouseY = event.clientY;
+
+      const position = haze.getBoundingClientRect();
+      const containerX = position.left + position.width/2;
+      const containerY = position.top + position.height/2;
+
+      const differenceX = (mouseX - containerX)/2;
+      const differenceY = (mouseY - containerY)/25;
+
+      const newX = Math.max(-1000, differenceX); 
+      const newY = Math.max(-1000, differenceY); 
+
+      haze.style.transform = `translate(${newX}px, ${newY}px)`;
+    }
+
+
+      document.addEventListener("mousemove", handleMouseMove);
   });
     
 </script>
 
 <div class="scroll-container">
   <div class="section">
+    <div bind:this={haze} class="haze"></div>
     <div class="border"></div>
     <div class="header-container">
       <h1 class="header-title">
